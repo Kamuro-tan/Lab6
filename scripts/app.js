3e7f1984fb4ebbabae82584f8591699aa96adad3
Kamuro-tan <>
2022-05-15 18:04:13 +0300

    Add 'tag' tab with ready massive organizedByTag.

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ var main = function(toDoObjects) {
     });
 
     var new_toDo = "";
-    
+
 
     $('.tabs a span').toArray().forEach(function(element) {
         $(element).on('click', function() {
@@ -31,6 +31,50 @@ var main = function(toDoObjects) {
                 });
 
             } else if ($element.parent().is(':nth-child(3)')) {
+                console.log("щелчок на вкладке Теги");
+                var organizedByTag = [
+                    {
+                        "name": "покупки",
+                        "toDos": ["Купить продукты"]
+                    },
+                    {
+                        "name": "рутина",
+                        "toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+                    },
+                    {
+                        "name": "писательство",
+                        "toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+                    },
+                    {
+                        "name": "работа",
+                        "toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник","Ответить на электронные письма", "Закончить писать книгу"]
+                    },
+                    {
+                        "name": " преподавание",
+                        "toDos": ["Подготовиться к лекции в понедельник"]
+                    },
+                    {
+                        "name": "питомцы",
+                        "toDos": ["Вывести Грейси на прогулку в парк "]
+                    }
+                ];
+
+                organizedByTag.forEach(function(tag) {
+                    var $tagName = $("<h3>").text(tag.name);
+                    $content = $("<ul>");
+
+                    tag.toDos.forEach(function (description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+                    
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+
+                });
+
+
+            } else if ($element.parent().is(':nth-child(4)')) {
                 $content = $('<form>');
                 $content.append($('<input>').attr({ type: 'text', 
                                                     placeholder: 'Введите новую задачу..', 
