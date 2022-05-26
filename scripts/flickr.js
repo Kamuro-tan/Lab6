01dc524267af25462b328f076e175e604fe6f5e6
Kamuro-tan <>
2022-05-26 18:01:47 +0300

    End 6th lab.

--- scripts/flickr.js
+++ scripts/flickr.js
@@ -1,31 +1,60 @@
 
-// setTimeout(function () {
-//     console.log("hello, world!");
-// }, 5000);
+var flickr_ajax = function(flickerAPI) {
+    var images = [];
 
+    $.ajax({
+        url: flickerAPI,
+        dataType: "jsonp", // jsonp
+        jsonpCallback: 'jsonFlickrFeed', // add this property
+        success: function (result, status, xhr) {
+            $.each(result.items, function (i, item) {
+                images.push(item.media.m);
+                if (i === 10) {
+                    return false;
+                }
+            });
+
+        },
+        error: function (xhr, status, error) {
+            console.log(xhr);
+            $(".flickr_slide_show").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText);
+        }
+    });
+
+    return images;
+}
 
 var main = function () {
-    var messages = ["первое сообщение", "второе сообщение", "третье", "четвертое"];
+    var flickerAPI = "//api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#flickr_search").val();
+    // var images = flickr_ajax(flickerAPI);
+    var images = [];
 
-    var displayMessage = function (messageIndex) {
-        var $message = $("<p>").text(messages[messageIndex]).hide();
-        $(".message").empty();
-        $(".message ").append($message);
-        $message.fadeIn();
+    var displayImage = function (imageIndex) {
+        var $img = $("<img>").attr("src", images[imageIndex]);
+        $('.flickr_slide_show').empty();
+        $('.flickr_slide_show').append($img);
+        $img.fadeIn();
 
         setTimeout(function () {
-            if (messageIndex < messages.length - 1) {
-                messageIndex = messageIndex + 1;
+            if (imageIndex < images.length - 1) {
+                imageIndex = imageIndex + 1;
             } else {
-                messageIndex = 0;
+                imageIndex = 0;
             }
 
-            displayMessage(messageIndex);
+            displayImage(imageIndex);
         }, 3000);
 
     };
 
-    displayMessage(0);
+    $("#flickr_submit").on('click', function() {
+        flickerAPI = "//api.flickr.com/services/feeds/photos_public.gne?format=json&tags=" + $("#flickr_search").val();
+        images = flickr_ajax(flickerAPI);
+        return false;
+    });
+
+    displayImage(0);
+
 }
 
 
